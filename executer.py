0f5a0af344c29666db21db05cab0c9c737485b39
unknown <alemon074@gmail.com>
2025-02-21 21:09:25 -0500

    generating the code for animating different chaotic curves!

--- executer.py
+++ executer.py
@@ -1,15 +1,18 @@
-
-
 # Developing the animation that compares chaotic curves
 # with very simmilar initial conditions. 
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from src.utils import logistic_eq_iterator
+
+# Number of iterations
+iterations = 100
 
 # Sample data
-x = [i for i in range(100)]
-y1 = [i**0.5 for i in x]  # Example data for the red curve
-y2 = [i**0.3 for i in x]  # Example data for the black curve
+x = [i for i in range(iterations)]
+y1 = logistic_eq_iterator(3.9999, 0.1, iterations)#[i**0.5 for i in x]
+y2 = logistic_eq_iterator(3.9998, 0.1, iterations)#[i**0.3 for i in x]  
+y3 = logistic_eq_iterator(3.2, 0.1, iterations)
 
 # Initialize the figure and axis
 # Here fig represents the entire figure in which the plot is drawn.
@@ -22,12 +25,13 @@ fig, ax = plt.subplots()
 # returns a tuple, because ax.plot() can plot multiple lines,
 # if we pass multiple parameters to it. In this case, cause we are
 # passing just 1 line, we just need the first element of the tuple.
-line1, = ax.plot(x, y1, 'r-', label='Red Curve')  # Red curve
-line2, = ax.plot(x, y2, 'k-', label='Black Curve')  # Black curve
+line1, = ax.plot(x, y1, 'r--', label='Chaotic red')  # Red curve
+line2, = ax.plot(x, y2, 'k-', label='Chaotic black')  # Black curve
+line3, = ax.plot(x, y3, 'g*-', label='Periodic')  # Black curve
 
 # Set up the plot limits
 ax.set_xlim(0, max(x))
-ax.set_ylim(0, max(max(y1), max(y2)))
+ax.set_ylim(0, max(max(y1)+0.1, max(y2)+0.1, max(y3)+0.1))
 ax.legend()
 
 # The update function is responsible for updating the data in the plot
@@ -38,7 +42,8 @@ ax.legend()
 def update(frame):
     line1.set_data(x[:frame], y1[:frame])
     line2.set_data(x[:frame], y2[:frame])
-    return line1, line2
+    line3.set_data(x[:frame], y3[:frame])
+    return line1, line2, line3
 
 # Create the animation
 # A frame: represents a single step in the animation, each frame 
@@ -48,11 +53,14 @@ def update(frame):
 ani = animation.FuncAnimation(
     fig, 
     update, 
-    frames= range(0, 100, 5), 
-    blit=True
+    frames= range(0, iterations, 1), 
+    blit=True,
+    interval=600 # delay btween frames in miliseconds
 )
 
 # Show the plot
 plt.show()
+
+
 
 
